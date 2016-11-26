cf1e5d66e9bde6127c76887770862ba1c2448009
Maxim <maxim_st@gmx.de>
2016-11-26 12:36:37 +0100

    Initial Commit

---
+++ PasafeCardCodes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PasafeCardCodes
+//
+//  Created by Maxim Strzebkowski on 26.11.16.
+//  Copyright © 2016 For Loop Studios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
